d20c65ae5c1c79a759686394ac07598c506c2038
Nikhil Rajpoot <51000636+23MANIK@users.noreply.github.com>
2021-05-17 22:53:40 +0530

    starting the server  and adding files

---
+++ server.js
@@ -0,0 +1,23 @@
+//getting Starting with required libraries
+const socketio=require('socket.io');
+const http=require('http')
+const express=require("express");
+const path=require('path');
+
+
+const app=express();
+const server=http.createServer(app);
+const io=socketio(server);
+
+//setting static path. Path module is used for handling and transforming file paths
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on('connection',socket=>{
+    console.log("connection started");
+});
+
+// Run server on env port and at local port
+const PORT=process.env.PORT||3000;
+server.listen(PORT,()=>{
+    console.log('server is running on port '+3000);
+});
