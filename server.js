560eb61ac1186e2f6d5f487b8bb3aedecae4e144
Nikhil Rajpoot <51000636+23MANIK@users.noreply.github.com>
2021-07-03 17:25:43 +0530

    adding data base to store the previous chats

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const express=require("express");
 const path=require('path');
 const formatMessage=require('./utils/messages');
 const {userJoin,getCurrentUser,userLeave,getTeamUsers}=require('./utils/users');
-
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ChatApp', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app=express();
 const server=http.createServer(app);
@@ -14,17 +14,44 @@ const io=socketio(server);
 
 //setting static path. Path module is used for handling and transforming file paths
 app.use(express.static(path.join(__dirname,'public')));
-
+ 
 const botName='sysMsg';
+
+const chatMsgSchema= new mongoose.Schema({
+    name:String,
+    msg:String,
+    team: String
+}, 
+{
+    timestamps: true
+});
+
+const chatMsg=mongoose.model("chatMsg",chatMsgSchema);
+
+// var chatmsg=new chatMsg({
+//     name: "rajpoot",
+//     msg:"king of world",
+//     team:"volleyball"
+// });
+
+// chatmsg.save();
+
+
 //Run when client connets
 io.on('connection',socket=>{
 
-    socket.on('joinTeam',({username,teams})=>{
+    socket.on('joinTeam', async ({username,teams})=>{
 
+        const msgs=await chatMsg.find({}); 
+        // console.log(msgs);
         const user=userJoin(socket.id,username,teams);
 
         socket.join(user.team);
 
+        for(let i = 0 ; i < msgs.length; i++) {
+            if(user.team===msgs[i].team)
+            socket.emit('message',formatMessage(msgs[i].name,msgs[i].msg));
+         }
         //Welcome current user
         socket.emit('message',formatMessage(botName,"Welcome to Sports team Chat"));
 
@@ -42,6 +69,12 @@ io.on('connection',socket=>{
     socket.on('chatMessage',msg=>{
         const user=getCurrentUser(socket.id);
 
+        var chatmsg=new chatMsg({
+            name: user.username,
+            msg: msg,
+            team: user.team
+        });
+        chatmsg.save();
         io.to(user.team).emit('message',formatMessage(user.username,msg));
     });
 
